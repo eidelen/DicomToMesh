d94746dd763332dcd00d40e24a25736fb7c75327
Adrian Schneider <adrian.schneider.pr@gmail.com>
2025-06-14 17:23:19 +0200

    Update test-ppa-dicom2mesh.sh

--- dicom2mesh/test/test-ppa-dicom2mesh.sh
+++ dicom2mesh/test/test-ppa-dicom2mesh.sh
@@ -13,7 +13,7 @@ sudo apt-get update -qq
 sudo apt-get install -y --no-install-recommends software-properties-common
 sudo add-apt-repository -y "$PPA"
 
-sudo apt-get install -y build-essential cmake libglvnd-dev libproj-dev libvtk9-dev libvtk9-qt-dev qt5-qmake qtbase5-dev-tools
+# sudo apt-get install -y build-essential cmake libglvnd-dev libproj-dev libvtk9-dev libvtk9-qt-dev qt5-qmake qtbase5-dev-tools
 
 # Step 2: Install dicom2mesh
 echo "Installing $PACKAGE"
